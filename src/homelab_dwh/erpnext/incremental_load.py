1786d9db549d77ae7f264495b02526e686655a77
Minh Ngo <ngonhatminh9898@gmail.com>
2026-04-20 20:19:38 +0930

    add incremental load pipline and change project structure

---
+++ src/homelab_dwh/erpnext/incremental_load.py
@@ -0,0 +1,43 @@
+import humanize
+import dlt
+from dlt.sources.sql_database import sql_database
+from prefect import flow, task, get_run_logger
+from prefect.blocks.system import Secret
+
+@task(name="erpenxt-incremental-load")
+def load_inrecemental_database() -> str:
+    source_conn = Secret.load("erpnext-mysql-conn").get()
+    dest_conn = Secret.load("postgres-dwh-conn").get()
+
+    """Use the sql_database source to completely load all tables in a database"""
+    pipeline = dlt.pipeline(
+        pipeline_name="erpnext_raw_to_dwh", 
+        destination=dlt.destinations.postgres(credentials=dest_conn), 
+        dataset_name="erpnext_raw")
+
+    # By default the sql_database source reflects all tables in the schema
+    # The database credentials are sourced from the `.dlt/secrets.toml` configuration
+    source = sql_database(credentials=source_conn ,engine_kwargs={
+        "connect_args":{
+            "init_command": "SET GLOBAL max_connections = 100000"
+        }   
+    })
+
+    # Run the pipeline. For a large db this may take a while
+    info = pipeline.run(source, write_disposition="merge")
+    duration = humanize.precisedelta(
+        pipeline.last_trace.finished_at - pipeline.last_trace.started_at
+    )
+    return f"{duration}\n{info}"
+
+@flow(name="erpnext-incremental-load",log_prints=True)
+def load_incremental_database_flow() -> None:
+    logger = get_run_logger()
+    logger.info("Starting ERPNext incremental load into Postgres DWH")
+    result = load_inrecemental_database()
+    logger.info(f"Load finished: {result} ")
+
+if __name__ == "__main__":
+    # Load all tables from the database.
+    # Warning: The sample database is very large
+    load_incremental_database_flow()
