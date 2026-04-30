1a572429347d19f2121ba7cd641a8705de9be9c6
Minh Ngo <ngonhatminh9898@gmail.com>
2026-04-30 21:48:54 +0930

    add profile.yml config

--- src/homelab_dwh/erpnext/transformation/flow_transform.py
+++ src/homelab_dwh/erpnext/transformation/flow_transform.py
@@ -1,10 +1,16 @@
 from prefect import flow
 from prefect_dbt import PrefectDbtRunner
+from prefect.blocks.system import Secret
+import os   
 
 
 @flow
 def run_dbt():
-    PrefectDbtRunner().invoke(["build"])
+
+    env_vars = Secret.load("dbt-profile").get()
+    os.environ.update(env_vars)
+
+    PrefectDbtRunner().invoke(["build", "--project-dir", "src/homelab_dwh/erpnext/transformation"])
 
 
 if __name__ == "__main__":
